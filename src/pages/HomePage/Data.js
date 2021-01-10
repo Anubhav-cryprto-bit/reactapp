201ae3d4614ac8644d12061f16bdf49edda58e16
Anubhav-cryprto-bit <ab2852@srmist.edu.in>
2021-01-10 20:55:23 +0530

    whatever

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,68 @@
+export const homeObjOne = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Machine-Learning Project',
+  headline: 'Customized ML-Model to Track Pulmonary Fibrosis',
+  description:
+    'Imagine one day, your breathing became consistently labored and shallow. Months later you were finally diagnosed with pulmonary fibrosis, a disorder with no known cause and no known cure, created by scarring of the lungs. If that happened to you, you would want to know your prognosis. That’s where a troubling disease becomes frightening for the patient: outcomes can range from long-term stability to rapid deterioration, but doctors aren’t easily able to tell where an individual may fall on that spectrum,Here is where our ML Model comes into play.',
+  buttonLabel: 'Get Started',
+  imgStart: '',
+  img: require('../../images/svg-1.svg'),
+  alt: 'Credit Card',
+  start: ''
+};
+
+export const homeObjTwo = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Instant Setup',
+  headline: 'User Friendly Interactive GUI with Real Time Data Visualizations',
+  description:
+    "We use Streamlit-Framework to design our ML-GUI,click on the Button Below to Know More",
+  buttonLabel: 'Learn More',
+  imgStart: '',
+  img: require('../../images/svg-2.svg'),
+  alt: 'Vault',
+  start: ''
+};
+
+export const homeObjThree = {
+  primary: false,
+  lightBg: true,
+  lightTopLine: false,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Software Used & Project-Type',
+  headline:
+    'Full Stack Web Application using Python & ReactJS Framework',
+  description:
+    "React (also known as React.js or ReactJS) is an open-source, front end, JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies.For Backend we go with Python as it is very convenient for coding ML Backends and sending JSON Requests",
+  buttonLabel: 'Learn More About React',
+  imgStart: 'start',
+  img: require('../../images/svg-12.svg'),
+  alt: 'Vault',
+  start: 'true'
+};
+
+export const homeObjFour = {
+  primary: true,
+  lightBg: false,
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Secure Database',
+  headline: 'All your data is stored on our secure server',
+  description:
+    'You will never have to worry about your information getting leaked. We will ensure your records are kept safe.',
+  buttonLabel: 'Learn More',
+  imgStart: 'start',
+  img: require('../../images/svg-3.svg'),
+  alt: 'Vault',
+  start: 'true'
+};
